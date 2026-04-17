f70fa808b164db5f99807367a3f10128964dd3b6
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-04-17 13:41:00 -0600

    retorno de funciones

--- proyectos/01-reforzamiento/src/bases/05-funciones.ts
+++ proyectos/01-reforzamiento/src/bases/05-funciones.ts
@@ -1,3 +1,8 @@
+interface User{
+    uid: string;
+    username: string;
+}
+
 //tipo 1 de declarar funciones normales
 function greet (name:string):string
 {
@@ -10,20 +15,37 @@ const greet2=(name:string):string => {
     return `Hola ${name}`
 }
 
+
+
+
 const messag2= greet("vegeta");
 
-function getUser():{}{
+function getUser():User{
     return{
         uid:"ABC-123",
         username:"EL_PAPI23"
     }
 }
 
-const getUser2=():{}=>{
-    return{
+
+
+//funcion de flecha simplificada
+const greet3=(name:string) => `Hola ${name}`;
+console.log(greet3("papi"))
+
+const getUser2=():User=>({
         uid:"ABC-123",
         username:"EL_PAPI23"
-    }
-}
+    });
+
 const user2= getUser2();
 console.log(user2)
+
+
+const myNumber:number[]=[1,2,3,4,5];
+
+myNumber.forEach(function(value){console.log({value});})
+
+myNumber.forEach((value)=>console.log({value}))
+
+myNumber.forEach(console.log)
