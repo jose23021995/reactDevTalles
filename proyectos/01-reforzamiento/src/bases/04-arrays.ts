ed6a85469b4635e169fe3c1e31c0a87b3316cb24
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-04-17 12:28:21 -0600

    arrays

---
+++ proyectos/01-reforzamiento/src/bases/04-arrays.ts
@@ -0,0 +1,11 @@
+const myArray:(number|string)[]=[1,2,3,4,5,6,7,"8"];
+// const myArray2=myArray;
+//const myArray2=[...myArray];
+const myArray2= structuredClone(myArray)
+
+myArray2.push(9)
+console.log({myArray,myArray2});
+
+// for(const myNumber of myArray){
+//     console.log(myNumber+10);
+// }
